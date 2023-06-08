717e1c8ebeba86fac150a59ed70c4cf3443f22e9
José Molina <jose.molina@parity.io>
2023-06-08 02:43:01 +0200

    add multipart to upload to ipfs

--- src/ipfs.rs
+++ src/ipfs.rs
@@ -1,12 +1,15 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
 use alloc::borrow::ToOwned;
+use alloc::format;
 use frame_support::log;
 use frame_support::sp_runtime::offchain::http;
 use frame_support::sp_runtime::offchain::http::Request;
 use sp_core::offchain::Duration;
 use sp_std::vec;
 use sp_std::vec::Vec;
+
+const BOUNDARY: &[u8] = b"------BOUNDARY";
 
 pub fn ipfs_get_hash_from_sha256(hash: &[u8; 32]) -> Vec<u8> {
     let full_data: Vec<_> = vec![vec![0x12, 0x20], hash.to_vec()]
@@ -16,11 +19,27 @@ pub fn ipfs_get_hash_from_sha256(hash: &[u8; 32]) -> Vec<u8> {
     bs58::encode(full_data).into_vec()
 }
 
+fn make_multipart(data: &[u8]) -> Vec<u8> {
+    BOUNDARY
+        .iter()
+        .chain(b"\nContent-Disposition: form-data; name=\"file\"\n\n")
+        .chain(data)
+        .chain(b"\n")
+        .chain(BOUNDARY)
+        .chain(b"--")
+        .copied()
+        .collect::<Vec<u8>>()
+}
+
 pub fn ipfs_upload(base_url: &str, data: &[u8]) -> Result<(), http::Error> {
     let url = base_url.to_owned() + "/api/v0/block/put";
-    let request = Request::post(&url, vec![data]);
+    let multipart = make_multipart(data);
+    let header =
+        format!("multipart/form-data; boundary={}", core::str::from_utf8(BOUNDARY).unwrap());
+    let request = Request::post(&url, vec![multipart.as_slice()]);
     let deadline = sp_io::offchain::timestamp().add(Duration::from_millis(5_000));
     let pending = request
+        .add_header("Content-Type", header.as_str())
         .deadline(deadline)
         .send()
         .map_err(|_| http::Error::IoError)?;
